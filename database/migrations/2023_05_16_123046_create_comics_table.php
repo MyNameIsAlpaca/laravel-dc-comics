f88fb82951ed27721afeb20aeea2c4d508cb5648
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-05-16 15:13:51 +0200

    feat: test seed

--- database/migrations/2023_05_16_123046_create_comics_table.php
+++ database/migrations/2023_05_16_123046_create_comics_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 200);
+            $table->text('desc');
+            $table->text('thumb');
+            $table->decimal('price', 5, 2);
+            $table->string('series', 50);
+            $table->string('sales_date', 11);
+            $table->string('type', 30);
+            $table->string('artists');
+            $table->string('writers');
             $table->timestamps();
         });
     }
--- database/seeders/ComicsSeeder.php
+++ database/seeders/ComicsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\comics;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,18 @@ class ComicsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $newComics = new comics();
+
+        $newComics->title = 'ciao';
+        $newComics->desc = 'descrizione';
+        $newComics->thumb = 'https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY';
+        $newComics->price = 12.00;
+        $newComics->series = 'xman';
+        $newComics->sales_date = '12-23-2023';
+        $newComics->type = 'horror';
+        $newComics->artists = 'jason paul';
+        $newComics->writers = 'jason paul';
+
+        $newComics->save();
     }
 }
